bc35a5a96eec90cee70f8359ccfa8fce2f16e161
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-18 21:37:00 +0000

    Localisation updates for extension messages from Betawiki (2008-02-18 21:13 CET)

--- wikihiero.i18n.php
+++ wikihiero.i18n.php
@@ -11,9 +11,24 @@ $messages['en'] = array(
 	'wikihiero-desc' => 'Adds <tt>&lt;hiero&gt;</tt> tag to display hieroglyph',
 );
 
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$messages['ar'] = array(
+	'wikihiero-desc' => 'يضيف الوسم <tt>&lt;hiero&gt;</tt> لعرض الهيروغليفية',
+);
+
 /** German (Deutsch)
  * @author Raimond Spekking
  */
 $messages['de'] = array(
 	'wikihiero-desc' => 'Fügt das <tt>&lt;hiero&gt;</tt>-Tag zur Formatierung von Hieroglyphen hinzu',
 );
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'wikihiero-desc' => 'Voegt de tag <tt>&lt;hiero&gt;</tt> toe voor de weergave van hiërogliefen',
+);
+
