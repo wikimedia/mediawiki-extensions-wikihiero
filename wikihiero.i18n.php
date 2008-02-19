5b7b2af152e70ce383c1a8aa652f7b9c12627972
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-19 13:22:00 +0000

    Localisation updates for extension messages from Betawiki (2008-02-19 14:10 CET)

--- wikihiero.i18n.php
+++ wikihiero.i18n.php
@@ -18,6 +18,13 @@ $messages['ar'] = array(
 	'wikihiero-desc' => 'يضيف الوسم <tt>&lt;hiero&gt;</tt> لعرض الهيروغليفية',
 );
 
+/** Czech (Česky)
+ * @author Matěj Grabovský
+ */
+$messages['cs'] = array(
+	'wikihiero-desc' => 'Přidává značku <tt>&lt;hiero&gt;</tt> pro zobrazování hieroglyfů',
+);
+
 /** German (Deutsch)
  * @author Raimond Spekking
  */
@@ -25,10 +32,66 @@ $messages['de'] = array(
 	'wikihiero-desc' => 'Fügt das <tt>&lt;hiero&gt;</tt>-Tag zur Formatierung von Hieroglyphen hinzu',
 );
 
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'wikihiero-desc' => 'Ajoute la balise <tt>&lt;hiero&gt;</tt> pour afficher les hiéroglyphes',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'wikihiero-desc' => 'Přidawa tafličku <tt>&lt;hiero&gt;</tt> za zwobraznjenje hieroglyfow',
+);
+
+/** Lithuanian (Lietuvių)
+ * @author Hugo.arg
+ */
+$messages['lt'] = array(
+	'wikihiero-desc' => 'Prideda <tt>&lt;hieroglifas&gt;</tt> tagus, kad rodyti hieroglifą',
+);
+
 /** Dutch (Nederlands)
  * @author Siebrand
  */
 $messages['nl'] = array(
 	'wikihiero-desc' => 'Voegt de tag <tt>&lt;hiero&gt;</tt> toe voor de weergave van hiërogliefen',
+);
+
+/** Portuguese (Português)
+ * @author Malafaya
+ */
+$messages['pt'] = array(
+	'wikihiero-desc' => 'Adiciona a marca <tt>&lt;hiero&gt;</tt> para apresentar hieróglifos',
+);
+
+/** Russian (Русский)
+ * @author .:Ajvol:.
+ */
+$messages['ru'] = array(
+	'wikihiero-desc' => 'Добавляет тег <tt>&lt;hiero&gt;</tt> для показа иероглифов',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'wikihiero-desc' => 'Pridáva značku <tt>&lt;hiero&gt;</tt> na zobrazovanie hieroglyfov',
+);
+
+/** Swedish (Svenska)
+ * @author Lejonel
+ */
+$messages['sv'] = array(
+	'wikihiero-desc' => 'Lägger till taggen <tt>&lt;hiero&gt;</tt> för att visa hieroglyfer',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Vinhtantran
+ */
+$messages['vi'] = array(
+	'wikihiero-desc' => 'Thêm thẻ <tt>&lt;hiero&gt;</tt> để hiển thị chữ tượng hình',
 );
 
