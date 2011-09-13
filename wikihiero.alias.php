04ae514f033f60c9e6e5d81748e85387cddec9b6
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-09-13 13:33:40 +0000

    Update for magic words and special pages localisation for extensions from http://translatewiki.net.

--- wikihiero.alias.php
+++ wikihiero.alias.php
@@ -12,3 +12,23 @@ $specialPageAliases = array();
 $specialPageAliases['en'] = array(
 	'Hieroglyphs' => array( 'Hieroglyphs' ),
 );
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'Hieroglyphs' => array( 'هيروغليفي' ),
+);
+
+/** Danish (Dansk) */
+$specialPageAliases['da'] = array(
+	'Hieroglyphs' => array( 'Hieroglyffer' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'Hieroglyphs' => array( 'Хиероглифи' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
