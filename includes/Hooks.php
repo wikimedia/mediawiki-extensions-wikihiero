71d0d977d3f7f01f4ba3ededeac79a742f09f078
Max Semenik <maxsem.wiki@gmail.com>
2019-11-14 21:19:24 -0800

    Stop passing objects by reference
    Bug: T193950
    Change-Id: Ia9975e5138aac67176393f5b2c81766bf4b6744e

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -9,10 +9,10 @@ class Hooks {
 	 * Because <hiero> tag is used rarely, we don't need to load its body on every hook call,
 	 * so we keep our simple hook handlers here.
 	 *
-	 * @param Parser &$parser
+	 * @param Parser $parser
 	 * @return bool
 	 */
-	public static function onParserFirstCallInit( &$parser ) {
+	public static function onParserFirstCallInit( $parser ) {
 		$parser->setHook( 'hiero',  WikiHiero::class . '::parserHook' );
 		return true;
 	}
