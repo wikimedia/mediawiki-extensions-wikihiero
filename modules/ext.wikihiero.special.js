ab2ccfc7bd63914a9f6ddeab062eacfa1278daaa
Ed Sanders <esanders@wikimedia.org>
2024-06-07 15:36:18 +0100

    build: Update eslint-config-wikimedia to 0.28.0
    Change-Id: Ibeaea8a31c7d677e62dc231406096a5211428516

--- modules/ext.wikihiero.special.js
+++ modules/ext.wikihiero.special.js
@@ -1,4 +1,4 @@
-$( function () {
+$( () => {
 	'use strict';
 
 	/* eslint-disable no-jquery/no-global-selector */
@@ -24,7 +24,7 @@ $( function () {
 		};
 		$.post( mw.util.wikiScript( 'api' ),
 			data,
-			function ( response ) {
+			( response ) => {
 				const html = '<table class="wikitable">' +
 					'<tr><th>' + mw.msg( 'wikihiero-input' ) + '</th><th>' +
 					mw.msg( 'wikihiero-result' ) + '</th></tr>' +
@@ -34,28 +34,28 @@ $( function () {
 					'<td>' + response.parse.text[ '*' ] + '</td></tr></table>';
 				$result.html( html );
 			}
-		).fail( function () {
+		).fail( () => {
 			$result.text( mw.msg( 'wikihiero-load-error' ) );
-		} ).always( function () {
+		} ).always( () => {
 			$result.css( 'opacity', 1 );
 		} );
 	}
 	submit.on( 'click', onSubmit );
 
 	// eslint-disable-next-line no-jquery/no-global-selector
-	$( '.mw-hiero-toc a' ).on( 'click', function ( e ) {
-		if ( this.hash ) {
+	$( '.mw-hiero-toc a' ).on( 'click', ( e ) => {
+		if ( e.target.hash ) {
 			e.preventDefault();
 			// eslint-disable-next-line no-jquery/no-global-selector
-			$( window ).scrollTop( $( this.hash ).offset().top - $( '.mw-hiero-form' ).outerHeight() );
+			$( window ).scrollTop( $( e.target.hash ).offset().top - $( '.mw-hiero-form' ).outerHeight() );
 		}
 	} );
 
 	// eslint-disable-next-line no-jquery/no-global-selector
-	$( '.mw-hiero-code' ).on( 'click', function () {
+	$( '.mw-hiero-code' ).on( 'click', ( e ) => {
 		const val = textarea.getValue().trim();
 		textarea.setValue(
-			val + ( val ? ' ' : '' ) + $( this ).find( '.mw-hiero-syntax' ).text()
+			val + ( val ? ' ' : '' ) + $( e.currentTarget ).find( '.mw-hiero-syntax' ).text()
 		);
 		textarea.moveCursorToEnd().focus();
 		onSubmit();
